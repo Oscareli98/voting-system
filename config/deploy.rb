4889217a9fb539383aefb339c113f61bf9767781
Oscar <newman.oscar@gmail.com>
2017-04-08 00:05:33 -0500

    updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/Oscareli98/voting-system.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/voting'
+set :deploy_to, '/var/www/vote.lasa2017.com'
 
 set :use_sudo, true
 set :linked_files, fetch(:linked_files, []).push('.env')
--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,5 +1,5 @@
 <nav class="purple">
   <div class="nav-wrapper container">
-    <a href="{{ url('/') }}" class="brand-logo">Prom 2016 Voting</a>
+    <a href="{{ url('/') }}" class="brand-logo">Prom 2017 Voting</a>
   </div>
 </nav>
